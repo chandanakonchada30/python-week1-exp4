40fa6fc3d82f8a5f8fefcd6ea725ea089645393b
chandanakonchada30 <24331a05e2@mvgrce.edu.in>
2025-12-12 14:22:08 +0530

    Add files via upload

---
+++ week1-4.py
@@ -0,0 +1,10 @@
+#24331A05E2
+#CALCULATION OF CIRCLE PARAMETERS, SIN FUNCTION, COS FUNCTION AND SQUARE ROOT USING MATH MODULE
+import math
+print("pi value is:", math.pi)
+radius=float(input("Enter radius: "))
+print("Area of circle is:", math.pi*radius*radius)
+print("Circumference of circle is:", 2*math.pi*radius)
+print("Square root of 225:", math.sqrt(225))
+print("cos value:",math.cos(45))
+print("sin value:",math.sin(45))
